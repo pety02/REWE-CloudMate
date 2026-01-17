6dea73fada53c5a6cdb73d94fe9b56f439a0f744
Petya Licheva <petya.licheva2002@abv.bg>
2026-01-17 23:00:14 +0200

    Search bar (#10)
    * routs fixed
    
    * search bar designed
    
    * search bar ts logic implemented and component tested
    
    ---------
    
    Co-authored-by: pety02 <pety02@abv.bg>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {AuthTabsComponent} from './auth/components/auth-tabs/auth-tabs.component';
+import {HomeViewComponent} from './home-view/home-view.component';
 
 export const routes: Routes = [
-  { path: '', component: AuthTabsComponent }
+  { path: '', component: AuthTabsComponent },
+  { path: 'home', component: HomeViewComponent }
 ];
