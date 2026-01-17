cdbfd88c7559e5257230abbcac891925de2e8b0b
pety02 <pety02@abv.bg>
2026-01-17 16:50:06 +0200

    login component's ts file + auth component implemented

---
+++ src/app/auth/login-card/login-card.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatButton} from '@angular/material/button';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatCard, MatCardContent, MatCardTitle} from '@angular/material/card';
+
+@Component({
+  selector: 'app-login-card',
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatFormField,
+    MatButton,
+    ReactiveFormsModule,
+    MatLabel,
+    MatInput,
+    MatCard,
+    MatCardTitle,
+    MatCardContent,
+  ],
+  templateUrl: './login-card.component.html',
+  styleUrl: './login-card.component.css'
+})
+export class LoginCardComponent {
+  loginForm: FormGroup<{
+    username: FormControl<string>;
+    password: FormControl<string>;
+  }> = new FormGroup({
+    username: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required}),
+    password: new FormControl ('', {
+      nonNullable: true,
+      validators: Validators.required}),
+  });
+
+  login(): void {
+
+  }
+}
