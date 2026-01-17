056310d5868fa7e4e55f3a1f17323fdebae099fe
pety02 <pety02@abv.bg>
2026-01-17 17:06:55 +0200

    register component implemented

---
+++ src/app/auth/register-card/register-card.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatCard, MatCardContent, MatCardTitle} from '@angular/material/card';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatButton} from '@angular/material/button';
+
+@Component({
+  selector: 'app-register-card',
+  imports: [
+    ReactiveFormsModule,
+    MatCardTitle,
+    MatCardContent,
+    MatLabel,
+    MatFormField,
+    MatInput,
+    MatButton,
+    MatCard
+  ],
+  templateUrl: './register-card.component.html',
+  styleUrl: './register-card.component.css'
+})
+export class RegisterCardComponent {
+  registerForm: FormGroup<{
+    username: FormControl<string>;
+    password: FormControl<string>;
+    confirmedPassword: FormControl<string>;
+  }> = new FormGroup({
+    username: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required}),
+    password: new FormControl ('', {
+      nonNullable: true,
+      validators: Validators.required}),
+    confirmedPassword: new FormControl ('', {
+      nonNullable: true,
+      validators: Validators.required}),
+  });
+
+  register(): void {
+
+  }
+}
