f866b0ba7f0d5acfc74f1b256f1904aab0b4c643
pety02 <pety02@abv.bg>
2026-01-25 14:28:34 +0200

    getters and setters of query as string implemented

--- src/app/file-card/file.service.ts
+++ src/app/file-card/file.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@angular/core';
-import {firstValueFrom, Observable, of} from 'rxjs';
+import {BehaviorSubject, firstValueFrom} from 'rxjs';
 import { FileItem } from './models/file-item.model';
 import {User} from '../auth/models/user.model';
 import {HttpClient} from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class FileService {
   private storageKey = 'files';
+  private searchQuery$ = new BehaviorSubject<string>('');
 
   constructor(private http: HttpClient) {
 
+  }
+
+  setSearchQuery(query: string): void {
+    this.searchQuery$.next(query.toLowerCase());
+  }
+
+  getSearchQuery() {
+    return this.searchQuery$.asObservable();
+  }
+
+  getCurrentSearchQuery(): string {
+    return this.searchQuery$.value;
   }
 
   public async initializeFiles() {
