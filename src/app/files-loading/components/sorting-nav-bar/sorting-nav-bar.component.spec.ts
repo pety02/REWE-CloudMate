9e70d4aa9c546b9f62cdd7cb1a3fd3ab2bc14f67
pety02 <pety02@abv.bg>
2026-01-26 22:16:01 +0200

    test sorting nav bar component

--- src/app/files-loading/components/sorting-nav-bar/sorting-nav-bar.component.spec.ts
+++ src/app/files-loading/components/sorting-nav-bar/sorting-nav-bar.component.spec.ts
@@ -1,9 +1,61 @@
+import { SortingNavBarComponent } from './sorting-nav-bar.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FileSortService } from '../../../../services/file-sort.service';
+
 describe('SortingNavBarComponent', () => {
-  it('should create the component', () => {});
+  let component: SortingNavBarComponent;
+  let fixture: ComponentFixture<SortingNavBarComponent>;
+  let mockFileSortService: jasmine.SpyObj<FileSortService>;
 
-  it('should toggle sort direction on same key click', () => {});
+  beforeEach(async () => {
+    mockFileSortService = jasmine.createSpyObj('FileSortService', ['setSort']);
 
-  it('should reset direction when changing sort key', () => {});
+    await TestBed.configureTestingModule({
+      imports: [SortingNavBarComponent],
+      providers: [
+        { provide: FileSortService, useValue: mockFileSortService }
+      ],
+    }).compileComponents();
 
-  it('should call sort service on applySort', () => {});
+    fixture = TestBed.createComponent(SortingNavBarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should toggle sort direction when clicking same sort key', () => {
+    component.activeSort = 'title';
+    component.sortDirection = 'asc';
+
+    component.onSortClick('title');
+
+    expect(component.sortDirection).toBe('desc');
+
+    component.onSortClick('title');
+
+    expect(component.sortDirection).toBe('asc');
+  });
+
+  it('should reset direction to asc when changing sort key', () => {
+    component.activeSort = 'title';
+    component.sortDirection = 'desc';
+
+    component.onSortClick('size');
+
+    expect(component.activeSort).toBe('size');
+    expect(component.sortDirection).toBe('asc');
+  });
+
+  it('should call sort service on applySort', () => {
+    component.activeSort = 'createdAt';
+    component.sortDirection = 'desc';
+
+    component.applySort();
+
+    expect(mockFileSortService.setSort)
+      .toHaveBeenCalledWith('createdAt', 'desc');
+  });
 });
